fe1860ee4bdae37dbe9ffd2af9b1768fae6638c2
Rohan <rohangawande2303@gmail.com>
2025-08-23 12:31:09 +0530

    added the google tag manager tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
+// app/layout.tsx
 import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import WhatsappButton from "../components/WhatsappButton";
 import CallButton from "../components/CallButton";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -47,8 +49,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} antialiased`}>
+      <head>
+        {/* Google Tag Manager (Head) */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WHQ6ZWLJ');`,
+          }}
+        />
+      </head>
       <body className="font-sans">
+        {/* Google Tag Manager (noscript - Body) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WHQ6ZWLJ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         {children}
+
         {/* Floating Buttons */}
         <WhatsappButton />
         <CallButton />
