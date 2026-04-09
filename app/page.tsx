0f1aaf7808d8215535a1fec4153ca10abc277641
vijay-panchal <panchal.viju@gmail.com>
2026-04-09 21:21:32 +0530

    url update https://enquiry.hubtown.co.in

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
       <Footer />
 
       {/* Floating Enquire Now Button */}
-      <EnquiryButton href="https://hubtown.co.in/projects_enquiry/trade-center/enquiry.html" />
+      <EnquiryButton href=" https://enquiry.hubtown.co.in/projects_enquiry/trade-center/enquiry.html" />
 
       <ContactModal
         isOpen={isModalOpen}
